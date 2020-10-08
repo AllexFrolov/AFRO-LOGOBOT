e92d1f0fff5b2b0347d7796398eca6949c959d66
MalahitTV <mosbka.37@yandex.ru>
2020-10-09 01:07:19 +0300

    delete write\read file. Added inline button. Added file upload when user clicks button

--- bot/telegram_bot.py
+++ bot/telegram_bot.py
@@ -1,23 +1,23 @@
-import cv2
-import numpy as np
-import telebot 
+import telebot
+from telebot import types
 import time
-import png
 from utils import add_text_to_img
 from generator_part.logo_gen import gen_logo_color
 from afro_postprocess import superresolute, imgfilter
-import matplotlib.pyplot as plt
+from PIL import Image
+import requests
+from config import token
 
-bot_token = "1225147524:AAHSRA9T2dmlSCmHJgtzWvjjlXpvk7OauKM"
-bot = telebot.TeleBot(token = bot_token)
+bot_token = token
+bot = telebot.TeleBot(bot_token)
 
 
-
-@bot.message_handler(commands = ['help', 'start'])
+@bot.message_handler(commands=['help', 'start'])
 def send_info(message):
-    if message.text=='/start':
-        bot.send_message(message.from_user.id, 'Привет!\nЯ умею генерировать логотипы. Давай сгенерируем логотип для твоей компании! 😋\nОтправь мне название твоей компании.')
-    elif message.text=='/help':
+    if message.text == '/start':
+        bot.send_message(message.from_user.id,
+                         'Привет!\nЯ умею генерировать логотипы. Давай сгенерируем логотип для твоей компании! 😋\nОтправь мне название твоей компании.')
+    elif message.text == '/help':
         bot.send_message(message.from_user.id, 'Отправь мне название твоей компании 👌🏿')
 
 
@@ -26,29 +26,27 @@ def company_receiving(message):
     print(message.text)
     # generating logo
     logo = gen_logo_color()
-    #applying superresolution and filtering
+    # applying superresolution and filtering
     logo = superresolute(logo)
     logo = imgfilter(logo)
     # adding company name on the image
     logo = add_text_to_img(message.text, logo)
+    logo = Image.fromarray(logo)
 
-    cv2.imwrite('image.jpg',logo)
-    image = cv2.imread('image.jpg')
-
-    # plt.imshow(image)
-    # plt.show()
-    
-    encoded_image = cv2.imencode('.jpeg', image)[1]
-    bytes_logo = encoded_image.tobytes()
-    # bytes_logo = open('image.png','rb')
-    # picture = cv2.imread('image.jpg')
-    # print(logo.shape)
-    # bytes_logo = logo.tobytes()
     # sending picture to user
-    bot.send_photo(message.from_user.id, bytes_logo, caption=message.text)
+    keyboard = types.InlineKeyboardMarkup()
+    url_button = types.InlineKeyboardButton(text="Примеры", callback_data='123')
+    keyboard.add(url_button)
+    bot.send_photo(message.from_user.id, logo, caption=message.text,
+                   reply_markup=keyboard)
 
 
-
+@bot.callback_query_handler(lambda query: query.data == '123')
+def process_callback_1(query):
+    bot.send_message(query.from_user.id, 'ЕЩе работаем над этим')
+    im_file = bot.get_file(query.message.json['photo'][0]['file_id'])
+    img = requests.get(f'https://api.telegram.org/file/bot{bot_token}/{im_file.file_path}')
+    bot.send_photo(query.from_user.id, img.content)
 
 
 while True:
